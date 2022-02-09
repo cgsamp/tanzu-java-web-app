812b98129f04ccc70e75ddc5fff4e1df2206dd97
Christopher Samp <chrisgsamp@gmail.com>
2022-02-09 15:24:36 -0500

    Updated hello controller

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Sampsoftware, Spring Boot + Tanzu, again";
+		return "Greetings from Sampsoftware, Spring Boot + Tanzu, Jon";
 	}
 
 }
