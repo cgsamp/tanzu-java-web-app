c466f4fcefc94796be63201ad98ab812270142c1
Chris Samp <chrisgsamp@gmail.com>
2023-05-17 16:44:51 -0500

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Tanzu, everyone!"));
+            .andExpect(content().string("Greetings from Tanzu, Chicago!"));
     }
 }
