b7edf2a7bac19d74879880d80a85c159d6fbdafc
Christopher Samp <chrisgsamp@gmail.com>
2023-07-12 13:12:06 -0500

    Changed message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Tanzu, Chicago!";
+		return "Greetings from Tanzu, Arlington Heihts!";
 	}
 
 }
